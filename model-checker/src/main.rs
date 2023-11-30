f3265607e390c78a93572f25c2fee6e92610cdc5
PFvanBeerendonk <64134830+PFvanBeerendonk@users.noreply.github.com>
2023-11-30 14:24:05 +0100

    git simple reader

--- model-checker/src/main.rs
+++ model-checker/src/main.rs
@@ -1,3 +1,29 @@
+use std::fs;
+use clap::Parser;
+
+/// definition of ARGS 
+// e.g.: `cargo run -- input/simple.ltl`
+#[derive(Parser)]
+struct Cli {
+    /// The path to the ltl file to read
+    ltl_file: std::path::PathBuf,
+}
+
 fn main() {
-    println!("Hello, world!");
+    let args = Cli::parse();
+
+    read_ltl_file(args.ltl_file);
+
+    println!("Terminated Succesfully");
+}
+
+fn read_ltl_file(file_path: std::path::PathBuf) {
+    // reads file of type ltl
+    println!("In file {:?}", file_path);
+
+    let contents = fs::read_to_string(file_path)
+        .expect("Should have been able to read the file");
+
+    println!("With text:\n{contents}");
+
 }
