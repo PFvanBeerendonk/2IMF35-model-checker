dbda4be3ab1289618bf6eb51eb1dee576fbeda8e
PFvanBeerendonk <64134830+PFvanBeerendonk@users.noreply.github.com>
2023-12-02 16:46:38 +0100

    file type validation

--- model-checker/src/main.rs
+++ model-checker/src/main.rs
@@ -1,16 +1,20 @@
 use std::fs;
 use clap::Parser;
 
+// END IMPORT
+
+
 /// definition of ARGS 
 // e.g.: `cargo run -- --file input/simple.ltl` --extended
 //       `main.exe -f input/simple.ltl -e`
 #[derive(Parser, Debug)]
 #[command(author, version, about, long_about = None)]
 struct Args {
-    /// The path to the ltl file to read
+    /// The path to the .ltl file to read
     #[arg(short, long)]
     file: std::path::PathBuf,
 
+    /// Use the extended algorithm, or the regular one
     #[arg(short, long, default_value_t=false)]
     extended: bool,
 }
@@ -24,9 +28,16 @@ fn main() {
     println!("\nTerminated Succesfully");
 }
 
+
+/**
+ * Read ltl file and convert to DataType
+ */
 fn read_ltl_file(file_path: std::path::PathBuf) {
-    // reads file of type ltl
-    println!("In file {:?}", file_path);
+    if "ltl" != file_path.extension().unwrap() {
+        panic!("File should have been of type .ltl");
+    }
+
+    println!("Unpacking file {:?}", file_path);
 
     let contents = fs::read_to_string(file_path)
         .expect("Should have been able to read the file");
