2e6d76941e76bd530728f0a2c82e8bfe5e5fee48
MaxOpperman <m.opperman98@gmail.com>
2023-12-06 12:53:27 +0100

    Eerste formule werkt 🥳

--- model-checker/src/types/formula.rs
+++ model-checker/src/types/formula.rs
@@ -15,7 +15,8 @@ pub enum Operator {
 #[derive(Debug)]
 pub enum Node {
     Variable(String), // X / Y / etc.
-    UnaryExpr { op: Operator, child: Box<Node> }, // SimpleFalse, SimpleTrue, Negate
+    Action(String), // Something from the set Act (i, plato, etc.)
+    UnaryExpr { op: Operator }, // SimpleFalse, SimpleTrue, Negate
     BinaryExpr { op: Operator, lhs: Box<Node>, rhs: Box<Node> }, 
 }
 
@@ -36,31 +37,50 @@ fn parse_logic(expression: &str) -> Node {
     let expression = expression.trim();
     let mut operator = Operator::SimpleFalse;
     if !expression.contains("(") {
-        println!("wtffff zit && in toch {}", expression);
-        if expression.contains("&&") {
-            operator = Operator::Conjunction;
-            if let Some(index) = expression.find("&&") {
-                return Node::BinaryExpr {
-                    op: operator,
-                    lhs: Box::new(Node::Variable(expression[..index].to_string())),
-                    rhs: Box::new(Node::Variable(expression[index + 2..].to_string()))
-                }
-            }
-        } else if expression.contains("||") {
-            operator = Operator::Disjunction;
-            if let Some(index) = expression.find("||") {
-                return Node::BinaryExpr {
-                    op: operator,
-                    lhs: Box::new(Node::Variable(expression[..index].to_string())),
-                    rhs: Box::new(Node::Variable(expression[index + 2..].to_string()))
-                }
-            }
+        let mut action = "";
+        let mut variable = "";
+        let mut operator = Operator::SimpleFalse;
+        if expression.contains("&&") || expression.contains("||") {
+            let (first_part, operator, second_part) = get_junctions(expression);
+            return Node::BinaryExpr {
+                op: operator,
+                lhs: Box::new(parse_logic(first_part)),
+                rhs: Box::new(parse_logic(second_part))
+            };
+        } else if expression.contains("[") && expression.contains("]") {
+            operator = Operator::BoxModality;
+            (action, variable) = extract_bracketed_strings(expression, "box");
+        } else if expression.contains("<") && expression.contains(">") {
+            operator = Operator::DiamondModality;
+            (action, variable) = extract_bracketed_strings(expression, "diamond");
+        } else {
+            println!("TODO {}", expression);
         }
-        println!("TODO {}", expression);
+        if variable == "true" {
+            return Node::BinaryExpr {
+                op: operator,
+                lhs: Box::new(Node::Action((*action).to_string())),
+                rhs: Box::new(Node::UnaryExpr{ op: Operator::SimpleTrue }),
+            };
+        } else if variable == "false" {
+            return Node::BinaryExpr {
+                op: operator,
+                lhs: Box::new(Node::Action((*action).to_string())),
+                rhs: Box::new(Node::UnaryExpr{ op: Operator::SimpleFalse }),
+            };
+        }
+        return Node::BinaryExpr {
+            op: operator,
+            lhs: Box::new(Node::Action((*action).to_string())),
+            rhs: Box::new(Node::Variable((*variable).to_string())),
+        };
     } else if expression.starts_with("(")  {
         let (first_part, operator, second_part) = get_junctions(expression);
-        println!("Part1: {:}, Part2: {:}, Operator {:?}", first_part, second_part, operator);
-        return Node::BinaryExpr { op: operator, lhs: Box::new(parse_logic(first_part)), rhs: Box::new(parse_logic(second_part)) };
+        return Node::BinaryExpr {
+            op: operator,
+            lhs: Box::new(parse_logic(first_part)),
+            rhs: Box::new(parse_logic(second_part))
+        };
     } else {
         let mut lhs = Node::Variable("TODO".to_string());
         let and_index = check_junction_before_paren(expression, "&&");
@@ -82,7 +102,30 @@ fn parse_logic(expression: &str) -> Node {
         } 
         if let Some(extracted) = extract_text_before_brackets(expression) {
             // do mu, nu, <>, [], &&, and ||
-            if extracted.starts_with("mu") {
+            if extracted.ends_with("&&") {
+                operator = Operator::Conjunction;
+                lhs = Node::Variable(extracted[..extracted.len()-2].to_string());
+            } else if extracted.ends_with("||") {
+                operator = Operator::Disjunction;
+                lhs = Node::Variable(extracted[..extracted.len()-2].to_string());
+            } else if extracted.starts_with("[") {
+                println!("EIGENLIJK NIET MOGELIJK {} en {}", expression, extracted);
+                let (action, variable) = extract_bracketed_strings(expression, "box");
+                return Node::BinaryExpr {
+                    op: Operator::BoxModality,
+                    lhs: Box::new(Node::Action((*action).to_string())),
+                    rhs: Box::new(parse_logic(variable)),
+                };
+            } else if extracted.starts_with("<") {
+                println!("EIGENLIJK NIET MOGELIJK {} en {}", expression, extracted);
+                let (action, variable) = extract_bracketed_strings(expression, "diamond");
+                return Node::BinaryExpr {
+                    op: Operator::DiamondModality,
+                    lhs: Box::new(Node::Action((*action).to_string())),
+                    rhs: Box::new(parse_logic(variable)),
+                };
+                // lhs = Node::Variable(extracted[0..3].to_string());
+            } else if extracted.starts_with("mu") {
                 operator = Operator::LeastFixpoint;
                 lhs = Node::Variable(extracted[2..3].to_string());
             } else if extracted.starts_with("nu") {
@@ -99,10 +142,10 @@ fn parse_logic(expression: &str) -> Node {
             }
         };
         if let Some(extracted) = extract_text_between_brackets(expression) {
-            println!("inner {}", extracted);
             return Node::BinaryExpr { op: operator, lhs: Box::new(lhs), rhs: Box::new(parse_logic(extracted)) };
         };
     }
+    println!("PANIC {}", expression);
     return Node::Variable("ERROR".to_string());
 }
 
@@ -114,23 +157,9 @@ fn extract_text_before_brackets(text: &str) -> Option<&str> {
 
 fn extract_text_between_brackets(text: &str) -> Option<&str> {
     let start_idx = text.find('(')?;
-    let mut open_count = 0;
+    let end_idx = text.rfind(')')?;
 
-    let end_idx = text[start_idx..]
-        .char_indices()
-        .find_map(|(i, c)| {
-            if c == '(' {
-                open_count += 1;
-            } else if c == ')' {
-                open_count -= 1;
-                if open_count == 0 {
-                    return Some(start_idx + i);
-                }
-            }
-            None
-        })?;
-
-    Some(&text[start_idx+1..=end_idx-1])
+    Some(&text[start_idx+1..end_idx])
 }
 
 fn get_strings_between_brackets(s: &str) -> Option<&str> {
@@ -159,10 +188,10 @@ fn get_strings_between_brackets(s: &str) -> Option<&str> {
     }
 
     if let (Some(open), Some(close)) = (open_index, close_index) {
-        if open > 0 {
+        if open > 2 {
             if Some(&s[open - 1..close])?.starts_with(".") {
                 // we know that it starts as a fixpoint (muV./nuV.) where V is a var
-                return Some(&s[open - 4..close]);
+                return Some(&s[open - 4..close + 1]);
             }
         }
         return Some(&s[open + 1..close]);
@@ -171,8 +200,8 @@ fn get_strings_between_brackets(s: &str) -> Option<&str> {
 }
 
 fn get_junctions<'a>(s: &'a str) -> (&'a str, Operator, &'a str) {
+    let mut operator = Operator::SimpleFalse;
     if let Some(first) = get_strings_between_brackets(s) {
-        let mut operator = Operator::SimpleFalse;
         if s[first.len()+2..].starts_with("&&") {
             operator = Operator::Conjunction;
         } else if s[first.len()+2..].starts_with("||") {
@@ -183,6 +212,12 @@ fn get_junctions<'a>(s: &'a str) -> (&'a str, Operator, &'a str) {
         if let Some(second) = get_strings_between_brackets(&s[first.len()..]) {
             return (first, operator, second);
         }
+    } else if s.contains("&&") {
+        let expressions: Vec<&str> = s.split("&&").collect();
+        return (expressions[0], Operator::Conjunction, expressions[1]);
+    } else if s.contains("||") {
+        let expressions: Vec<&str> = s.split("||").collect();
+        return (expressions[0], Operator::Disjunction, expressions[1]);
     }
     panic!("No conjunction or disjunction found");
 }
@@ -207,6 +242,33 @@ fn remove_brackets(mut input_string: String) -> String {
         }
     }
     input_string
+}
+
+fn extract_bracketed_strings<'a>(
+    input: &'a str,
+    bracket_type: &str,
+) -> (&'a str, &'a str) {
+    let open_bracket = match bracket_type {
+        "diamond" => '<',
+        "box" => '[',
+        _ => return ("", ""), // If an invalid bracket type is provided, return empty strings
+    };
+
+    if let Some(open_idx) = input.find(open_bracket) {
+        let close_bracket = match open_bracket {
+            '[' => ']',
+            '<' => '>',
+            _ => return ("", ""), // Invalid bracket type provided
+        };
+
+        if let Some(close_idx) = input.find(close_bracket) {
+            let bracket = &input[open_idx + 1..close_idx];
+            let content = &input[close_idx + 1..];
+            return (bracket, content);
+        }
+    }
+
+    ("", "")
 }
 
 impl Formula {
