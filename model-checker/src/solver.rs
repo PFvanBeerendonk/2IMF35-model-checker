6ac1b8d6551589b05455b39f13ed5138d7c4c2c8
RIck Overmeer <r.j.overmeer@student.tue.nl>
2023-12-12 12:33:36 +0100

    The basic algo should theoretically work (fingers crossed)

--- model-checker/src/solver.rs
+++ model-checker/src/solver.rs
@@ -8,8 +8,9 @@ use std::collections::HashSet;
 
 
 // Given a formula `f`, evaluate it over `ltl`
-pub fn execute(f: Formula, instance:Ltl) {
-    println!("Initializing");
+pub fn execute(f: Formula, instance:Ltl) -> HashSet<i64> {
+    let mut variable_map: HashMap<String,HashSet<i64>> = HashMap::new();
+    return eval(f.root_node, &instance, &mut variable_map);
     // let mut set = init(f, instance);
     
 }
@@ -18,7 +19,13 @@ fn eval(node: Node, instance:&Ltl, variable_map: &mut HashMap<String,HashSet<i64
     match node {
         Node::Variable(string) => {
             // X_i, TODO not sure about array yet.
-            return HashSet::new();
+            if (*variable_map).contains_key(&string.clone()) {
+                return retrieve_element(variable_map.get(&string.clone())).clone();
+            } else {
+                // TODO: Look at, do we actually want to insert this element? 
+                (*variable_map).insert(string.clone(), HashSet::new());
+                return retrieve_element(variable_map.get(&string.clone())).clone();
+            }
         }
         Node::BinaryExpr { op, lhs, rhs } => {
             if op == Operator::Conjunction {
@@ -31,23 +38,46 @@ fn eval(node: Node, instance:&Ltl, variable_map: &mut HashMap<String,HashSet<i64
                 let eval_rhs = eval(*rhs, instance, variable_map);
                 return eval_lhs.union(&eval_rhs).map(|x| *x).collect::<HashSet<i64>>();
             } else if op == Operator::LeastFixpoint {
-                
+                let node = *lhs;
+                match node {
+                    Node::Variable(string) => {
+                        (*variable_map).insert(string.clone(), HashSet::new());
+                        return calculate_fixpoint(string, *rhs, instance, variable_map)
+                    }
+                    Node::Action(_) => unreachable!(),
+                    Node::UnaryExpr { op: _ } => unreachable!(),
+                    Node::BinaryExpr { op: _, lhs: _, rhs: _ } => unreachable!(),
+                }
             } else if op == Operator::GreatestFixpoint {
                 let node = *lhs;
                 match node {
                     Node::Variable(string) => {
                         (*variable_map).insert(string.clone(), instance.get_all_states());
-                        let mut x_prime: HashSet<i64> = HashSet::new(); 
-                        let mut a = retrieve_element(variable_map.get(&string.clone())).clone();
-                        while (x_prime != a){
-                            x_prime = a.clone();
-                            let temp = eval(*rhs.clone(), instance, variable_map);
-                            (*variable_map).insert(string.clone(), temp);
-                            a = retrieve_element(variable_map.get(&string.clone())).clone();
-                        }
-                        return retrieve_element(variable_map.get(&string.clone())).clone();
+                        return calculate_fixpoint(string, *rhs, instance, variable_map)
                     }
                     Node::Action(_) => unreachable!(),
+                    Node::UnaryExpr { op: _ } => unreachable!(),
+                    Node::BinaryExpr { op: _, lhs: _, rhs: _ } => unreachable!(),
+                }
+            } else if op == Operator::DiamondModality {
+                let node = *lhs;
+                match node {
+                    Node::Action(string) => {
+                        let states_rhs: HashSet<i64> = eval(*rhs, instance, variable_map);
+                        return instance.get_diamond_modality(string, states_rhs)
+                    }
+                    Node::Variable(_) => unreachable!(),
+                    Node::UnaryExpr { op: _ } => unreachable!(),
+                    Node::BinaryExpr { op: _, lhs: _, rhs: _ } => unreachable!(),
+                }
+            } else if op == Operator::BoxModality {
+                let node = *lhs;
+                match node {
+                    Node::Action(string) => {
+                        let states_rhs: HashSet<i64> = eval(*rhs, instance, variable_map);
+                        return instance.get_diamond_modality(string, states_rhs)
+                    }
+                    Node::Variable(_) => unreachable!(),
                     Node::UnaryExpr { op: _ } => unreachable!(),
                     Node::BinaryExpr { op: _, lhs: _, rhs: _ } => unreachable!(),
                 }
@@ -73,9 +103,8 @@ fn eval(node: Node, instance:&Ltl, variable_map: &mut HashMap<String,HashSet<i64
             }
             return HashSet::new();
         }
-        Node::Action(string) => {
-            // TODO
-            return HashSet::new();
+        Node::Action(_) => {
+            unreachable!("Should not happen");
         }
     }
 }
@@ -89,6 +118,18 @@ fn retrieve_element(element: Option<&HashSet<i64>>) -> &HashSet<i64> {
             panic!("Should not happen");
         }
     }
+}
+
+fn calculate_fixpoint(string: String, g: Node, instance:&Ltl, variable_map: &mut HashMap<String,HashSet<i64>>) -> HashSet<i64> {
+    let mut x_prime: HashSet<i64> = HashSet::new(); 
+    let mut a = retrieve_element(variable_map.get(&string.clone())).clone();
+    while (x_prime != a){
+        x_prime = a.clone();
+        let temp = eval(g.clone(), instance, variable_map);
+        (*variable_map).insert(string.clone(), temp);
+        a = retrieve_element(variable_map.get(&string.clone())).clone();
+    }
+    return retrieve_element(variable_map.get(&string.clone())).clone();
 }
 
 pub fn execute_improved(f: Formula, instance:Ltl) {
