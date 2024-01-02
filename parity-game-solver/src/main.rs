070e4561309f7006e5db2ec8ee1220c3347fedca
PFvanBeerendonk <64134830+PFvanBeerendonk@users.noreply.github.com>
2024-01-02 14:59:58 +0100

    add input

--- parity-game-solver/src/main.rs
+++ parity-game-solver/src/main.rs
@@ -1,3 +1,45 @@
+use std::fs;
+use clap::Parser;
+
+// END IMPORT
+
+
+/// definition of ARGS 
+// e.g.: `cargo run -- --aut-file ../input/part2-1/dining_2.aut --mcf-file ../input/part2-1/invariantly_inevitably_eat.mcf --improved`
+//       `main.exe --aut-file ../input/part2-1/dining_2.aut --mcf-file ../input/part2-1/invariantly_inevitably_eat.mcf -i`
+#[derive(Parser, Debug)]
+#[command(author, version, about, long_about = None)]
+struct Args {
+    /// The path to the .gm file
+    #[arg(short, long)]
+    gm_file: std::path::PathBuf,
+
+    
+
+
+    /// Print debug data
+    #[arg(short, long, default_value_t=false)]
+    debug: bool,
+
+    /// Test if state `test_state` is in the output
+    #[arg(short, long, default_value_t=-1)]
+    test_state: i64,
+}
+
+
 fn main() {
-    println!("Hello, world!");
+    // Parse the arguments:
+    let args: Args = Args::parse();
+
+    let f: String = read_gm_file(args.gm_file, args.debug);
+
+
+
+    println!("\nTerminated Succesfully");
+
+}
+
+
+fn read_gm_file(file_path: std::path::PathBuf, debug:bool) -> String {
+    return String::new()
 }
