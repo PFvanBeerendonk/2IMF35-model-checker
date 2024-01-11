244f1dc99b9bd4220dbef55b58f1f11b9ee3c49a
MaxOpperman <m.opperman98@gmail.com>
2024-01-11 13:20:18 +0100

    Let whole folder of input files be run for bench

--- parity-game-solver/src/main.rs
+++ parity-game-solver/src/main.rs
@@ -10,6 +10,9 @@ use solver::{main_algo};
 use types::progress_measure::ProgressMeasure;
 use types::vertex::Vertex;
 use types::vertex::Vertices;
+use walkdir::WalkDir;
+use std::path::PathBuf;
+use std::borrow::Cow;
 // END IMPORT
 
 
@@ -21,44 +24,67 @@ use types::vertex::Vertices;
 struct Args {
     /// The path to the .gm file
     #[arg(short, long)]
-    gm_file: std::path::PathBuf,
+    gm_file: Option<std::path::PathBuf>,
+
+    /// The path to the folder with all the .gm files
+    #[arg(short, long, conflicts_with="gm_file")]
+    folder: Option<std::path::PathBuf>,
+
+    /// Only if there is a folder specified; use all the folowing lifting strategies
+    #[arg(short, long, default_values_t=vec![0,1,2,3,4], conflicts_with="lifting_strategy")]
+    multiple_lifting_strategies: Vec<i64>,
+
+    /// Where the output folder is located, emtpy if there should be no output
+    #[arg(short, long)]
+    output: Option<std::path::PathBuf>,
 
     /// Which lifting strategy is used.
     /// 0 for "input order" lifting strategy
     /// 1 for "random order" lifting strategy
     /// 2 for "least successors" lifting strategy
     /// 3 for fourth lifting strategy TODO
-    #[arg(short, long, default_value_t=0, verbatim_doc_comment)]
+    #[arg(short, long, default_value_t=0, verbatim_doc_comment, conflicts_with="multiple_lifting_strategies")]
     lifting_strategy: i64,
 
     /// Give a default seed, only used if `lifting_strategy` is 1 (random order)
     #[arg(short, long, default_value_t=1234)]
     seed: i64,
+
+    /// Whether the debug output should be printed
+    #[arg(short, long, default_value_t=false)]
+    debug: bool,
 }
 
 
 fn main() {
     // Parse the arguments:
     let args: Args = Args::parse();
-
-    let result = read_gm_file(args.gm_file);
-    println!("\n###   Finished Construction   ###\n");
-
-    let pm:ProgressMeasure = result.0;
-    let vertices: Vertices = result.1;
-    let d: i64 = result.2;
-
-    // set a seed
-    let seed;
-    if args.lifting_strategy == 1 {
-        seed = Some(args.seed);
-    } else {
-        seed = None
+    if args.gm_file.is_some() {
+        let result = read_gm_file(args.gm_file.unwrap());
+        
+        if args.debug {
+            println!("\n###   Finished Construction   ###\n");
+        }
+        let pm:ProgressMeasure = result.0;
+        let vertices: Vertices = result.1;
+        let d: i64 = result.2;
+    
+        // set a seed
+        let seed;
+        if args.lifting_strategy == 1 {
+            seed = Some(args.seed);
+        } else {
+            seed = None
+        }
+    
+        main_algo(pm, &vertices, d, args.lifting_strategy, seed, None, args.debug);
+    
+        if args.debug {
+            println!("\n###   Terminated Succesfully   ###\n");
+        }
+    } else if args.folder.is_some() {
+        run_folder(&args);
     }
-
-    main_algo(pm, &vertices, d, args.lifting_strategy, seed);
-
-    println!("\n###   Terminated Succesfully   ###\n");
 }
 
 
@@ -130,4 +156,63 @@ fn read_gm_file(file_path: std::path::PathBuf) -> (ProgressMeasure, Vertices, i6
 
 fn to_int64(f: &str) -> i64 {
     return f.parse::<i64>().unwrap()
+}
+
+
+fn run_folder(args: &Args) {
+    let mut folder_name = "".to_string();
+    if let Some(folder) = &args.folder {
+        for entry in WalkDir::new(folder).into_iter().filter_map(|e| e.ok()) {
+            if let Some(gm_file) = entry.file_name().to_str() {
+                if gm_file.ends_with(".gm") {
+                    println!("gm file {}", gm_file);
+                    // read the gm file
+                    let result = read_gm_file(entry.path().to_path_buf());
+                    if args.debug {
+                        println!("\n###   Finished Construction   ###\n");
+                    }
+                    let pm: ProgressMeasure = result.0;
+                    let vertices: Vertices = result.1;
+                    let d: i64 = result.2;
+                
+                    // set a seed
+                    let seed;
+                    seed = Some(args.seed);
+
+                    // loop over all the lifting strategies
+                    for lifting_strat in args.multiple_lifting_strategies.iter() {
+                        
+                        let output = PathBuf::from(
+                            [
+                                args.output.clone().unwrap_or_default().to_string_lossy().into(),
+                                folder_name.clone(),
+                                "/".to_string(),
+                                Cow::Borrowed(&gm_file[0..gm_file.len() - 3]).to_string(),
+                                "-lifting-".to_string(),
+                                lifting_strat.to_string(),
+                                ".txt".to_string(),
+                            ]
+                            .iter()
+                            .map(|s| s.to_string())
+                            .collect::<String>(),
+                        );
+                        if !std::path::Path::new(&output).exists() {
+                            let _ = fs::create_dir_all([
+                                args.output.clone().unwrap_or_default().to_string_lossy().into(),
+                                folder_name.clone(),
+                                ]
+                                .iter().map(|s| s.to_string()).collect::<String>());
+                        }
+
+                        main_algo(pm.clone(), &(vertices.clone()), d, *lifting_strat, seed, Some(output.clone()), args.debug);
+                        if args.debug {
+                            println!("\n###   Terminated Succesfully   ###\n");
+                        }
+                    }
+                } else {
+                    folder_name = entry.file_name().to_string_lossy().to_string();
+                }
+            }
+        }
+    }
 }
--- parity-game-solver/src/solver.rs
+++ parity-game-solver/src/solver.rs
@@ -1,14 +1,21 @@
 use crate::types::progress_measure::ProgressMeasure;
 use crate::types::vertex::Vertices;
-use crate::types::lifting_strategies::{least_successor_order, FocusListLiftingStrategy, PredecessorLiftingStrategy};
+use crate::types::lifting_strategies::{least_successor_order, most_successor_order, PredecessorLiftingStrategy};
 
+use std::fs::File;
+use std::io::{BufWriter, Write};
+use std::path::PathBuf;
 use permutation_iterator::Permutor;
 use std::collections::HashMap;
+use std::time::Instant;
 
-pub fn main_algo(progress_measure: ProgressMeasure, vertices: &Vertices, d: i64, lifting_strategy: i64, seed: Option<i64>) {
+pub fn main_algo(progress_measure: ProgressMeasure, vertices: &Vertices, d: i64, lifting_strategy: i64, seed: Option<i64>, output: Option<PathBuf>, debug: bool) {
+    let mut total_lifts = 0;
+    let mut successful_lifts = 0;
+
     // if seed is not set, we follow  order of `vertices`. Otherwise we will use a random seed based function
 
-    let mut pm: ProgressMeasure = progress_measure.clone();
+    let mut pm: ProgressMeasure = progress_measure;
 
     // let mut v: Vertex = vertices[0];
     let mut id: i64 = 0;
@@ -23,6 +30,9 @@ pub fn main_algo(progress_measure: ProgressMeasure, vertices: &Vertices, d: i64,
     // the vertex identifiers sorted based on the least successor lifting strategy
     let mut least_successor_lifting_strat = Default::default();
     let mut predecessor_lifting_strat = Default::default();
+
+    // measure the time that the program runs
+    let now = Instant::now();
     // if a seed is provided, we will make a hashed iterator
     if lifting_strategy == 1 {
         iter = Some(Permutor::new_with_u64_key(length, seed.unwrap() as u64));
@@ -65,21 +75,21 @@ pub fn main_algo(progress_measure: ProgressMeasure, vertices: &Vertices, d: i64,
     }
 
     loop {
-        print!("{:?}, ", id);
         result = pm.lift_v(id, &vertices, d);
+        total_lifts += 1;
         pm = result.0;
         did_update = result.1;
 
         // check if ϱ < Liftv (ϱ) for vertex v
         if did_update {
             did_update_this_master_loop = true;
+            successful_lifts += 1;
         }
         // if the lifting strategy is based on the given input order
-        if lifting_strategy == 0 || lifting_strategy == 3 {
+        if lifting_strategy == 0 {
             id += 1;
             if id == vertices.len() as i64 {
                 id = 0;
-                println!("reset!");
                 loop_end = true; // check final loop termination
             }
         } else if lifting_strategy == 2 {
@@ -87,10 +97,20 @@ pub fn main_algo(progress_measure: ProgressMeasure, vertices: &Vertices, d: i64,
                 Some(x) => id = x as i64,
                 None => {
                     // hard reset the iterator
-                    println!("reset!");
                     least_successor_lifting_strat = least_successor_order(&vertices).into_iter();
             
                     id = least_successor_lifting_strat.next().unwrap() as i64;
+                    loop_end = true; // check final loop termination
+                },
+            }
+        } else if lifting_strategy == 3 {
+            match most_successor_lifting_strat.next() {
+                Some(x) => id = x as i64,
+                None => {
+                    // hard reset the iterator
+                    most_successor_lifting_strat = most_successor_order(&vertices).into_iter();
+            
+                    id = most_successor_lifting_strat.next().unwrap() as i64;
                     loop_end = true; // check final loop termination
                 },
             }
@@ -99,7 +119,6 @@ pub fn main_algo(progress_measure: ProgressMeasure, vertices: &Vertices, d: i64,
                 Some(x) => id = x.identifier as i64,
                 None => {
                     // hard reset the iterator
-                    println!("reset!");
                     let top: HashMap<i64, bool> = pm.data.iter().enumerate()
                         .filter_map(|(i, row)| if row.is_none() { Some((i as i64, true)) } else { None })
                         .collect();
@@ -114,7 +133,6 @@ pub fn main_algo(progress_measure: ProgressMeasure, vertices: &Vertices, d: i64,
                 Some(x) => id = x as i64,
                 None => {
                     // hard reset the iterator
-                    println!("reset!");
                     iter = Some(Permutor::new_with_u64_key(length, seed.unwrap() as u64));
             
                     id = iter.as_mut().unwrap().next().unwrap() as i64;
@@ -136,25 +154,51 @@ pub fn main_algo(progress_measure: ProgressMeasure, vertices: &Vertices, d: i64,
         }
     }
 
-
     // checking stuff
-    println!("\n###   Finished Calculations   ###\n");
+    let elapsed = now.elapsed();
+    println!("\n###   Finished Calculations   ### (for {} in {:.4?})\n", output.clone().expect("no output file").as_path().display().to_string(), elapsed);
+    
 
     let non_t_count = pm.data.iter().flatten().count();
     let size = pm.data.iter().count();
-    println!("Size:         {}", size);
-    println!("Nr of non-T:  {}", non_t_count);
-    println!("Nr of T:      {}", size - non_t_count);
+    if output.is_some() {
+
+        let file = File::create(output.expect("Output file name could not be read").as_path()).expect("Unable to create file");
+        let mut f = BufWriter::new(file);
+
+        
+        write!(f, "Size:          {}\n", size).expect("unable to write");
+        write!(f, "Nr of non-T:   {}\n", non_t_count).expect("unable to write");
+        write!(f, "Nr of T:       {}\n", size - non_t_count).expect("unable to write");
+        write!(f, "Time:          {:.4?}\n", elapsed).expect("unable to write");
+        write!(f, "Nr of Lifts:   {}\n", total_lifts).expect("unable to write");
+        write!(f, "Success lifts: {}\n", successful_lifts).expect("unable to write");
+        write!(f, "\n\n{: <10} | ϱ()\n", "ID").expect("unable to write");
+        write!(f, "-----------+------------------------------------\n").expect("unable to write");
+        for (i, row) in pm.data.iter().enumerate()  {
+            if row.is_none() {
+                write!(f, "{: <10} | T\n", i).expect("unable to write");
+            } else {
+                write!(f, "{: <10} | {:?}\n", i, row.clone().unwrap()).expect("unable to write");
+            }
+        }
+    }
+
+    if debug {
+        println!("Size:         {}", size);
+        println!("Nr of non-T:  {}", non_t_count);
+        println!("Nr of T:      {}", size - non_t_count);
 
 
 
-    println!("\n\n{: <10} | ϱ()", "ID");
-    println!("-----------+------------------------------------");
-    for (i, row) in pm.data.iter().enumerate()  {
-        if row.is_none() {
-            println!("{: <10} | T", i);
-        } else {
-            println!("{: <10} | {:?}", i, row.clone().unwrap());
+        println!("\n\n{: <10} | ϱ()", "ID");
+        println!("-----------+------------------------------------");
+        for (i, row) in pm.data.iter().enumerate()  {
+            if row.is_none() {
+                println!("{: <10} | T", i);
+            } else {
+                println!("{: <10} | {:?}", i, row.clone().unwrap());
+            }
         }
     }
 }
